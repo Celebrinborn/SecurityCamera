603b682e802ef05a7d828fc40b929bd54e232794
Celebrinborn <cameronjunkmail1@gmail.com>
2023-12-29 21:40:08 -0800

    Add dataclass imports and update Detection and DetectionResult classes

--- object_detection/app.py
+++ object_detection/app.py
@@ -1,3 +1,4 @@
+import dataclasses
 from math import log
 from optparse import Option
 import os
@@ -7,7 +8,7 @@ import queue
 import re
 import threading
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from charset_normalizer import detect
 from kafka import KafkaConsumer, KafkaProducer
 from kafka.consumer.fetcher import ConsumerRecord
@@ -15,6 +16,10 @@ from kafka.errors import KafkaError
 from avro.io import DatumWriter, DatumReader, BinaryEncoder, BinaryDecoder
 from avro.datafile import DataFileReader, DataFileWriter
 from avro import schema
+
+import json
+from numpy import byte
+from sympy import fu, im
 
 
 from transformers import YolosImageProcessor, YolosForObjectDetection, PreTrainedModel
@@ -24,7 +29,8 @@ import torch
 from typing import List, Optional
 
 # Set up logging
-logging.basicConfig(level=logging.DEBUG)
+from log_config import configure_logging
+configure_logging()
 logger = logging.getLogger()
 
 # mute logging from kafka to exception and above only
@@ -38,7 +44,7 @@ class MotionMessageQueueItem:
     priority: float
     guid: str
     creation_timestamp: float
-    frame_jpg: bytes
+    frame_jpg: Image.Image
     motion_amount: float
     timeout: int
 
@@ -79,7 +85,7 @@ class MotionMessageQueueItem:
             priority = avro_data['priority']
             guid = avro_data['guid']
             creation_timestamp = avro_data['creation_timestamp']
-            frame_jpg = avro_data['frame_jpg']
+            frame_jpg = Image.open(io.BytesIO(avro_data['frame_jpg']))
             motion_amount = avro_data['motion_amount']
             timeout = avro_data['timeout']
         except KeyError as e:
@@ -99,38 +105,51 @@ class MotionMessageQueueItem:
 # Dataclass for YOLOs output
 @dataclass
 class Detection:
-    bounding_box: tuple  # You can use a namedtuple if needed
+    bounding_box: tuple[int, int, int, int]  # You can use a namedtuple if needed
     classification: str
     certainty: float
-    # # example of how to render the image with bounding boxes
-    # import cv2
-    # import numpy as np
-    # # convert bytes to numpy array
-    # nparr = np.frombuffer(detection_result.jpg, np.uint8)
-    # # decode image
-    # img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        # # example of how to render the image with bounding boxes
+        # def render(self, detection_result: DetectionResult):
+            # import cv2
+            # import numpy as np
+            # # convert bytes to numpy array
+            # nparr = np.frombuffer(detection_result.jpg, np.uint8)
+            # # decode image
+            # img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    # # draw bounding boxes
-    # # Print the values of detection.bounding_box
-    # for detection in detection_result.detections:
-    #     x1, y1, x2, y2 = map(int, detection.bounding_box)
-    #     cv2.rectangle(img_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            # # draw bounding boxes
+            # # Print the values of detection.bounding_box
+            # for detection in detection_result.detections:
+            #     x1, y1, x2, y2 = map(int, detection.bounding_box)
+            #     cv2.rectangle(img_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    #     cv2.putText(img_np, detection.classification, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            #     cv2.putText(img_np, detection.classification, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-    # # show image
-    # cv2.imshow("image", img_np)
-    # cv2.waitKey(1)
+            # # show image
+            # cv2.imshow("image", img_np)
+            # cv2.waitKey(1)
+    def to_dict(self):
+        return asdict(self)
+        
 
 @dataclass
 class DetectionResult:
     frame_id: str
     camera_name: str
-    jpg: bytes
+    jpg: Image.Image
     detections: List[Detection]
 
     def __repr__(self) -> str:
         return f'detection_count: {self.frame_id}: {len(self.detections)})'
+
+    def to_dict(self):
+        assert isinstance(self.jpg, bytes), f"Unexpected type for jpg: {type(self.jpg)}"
+        return {
+            "frame_id": self.frame_id,
+            "camera_name": self.camera_name,
+            "jpg": self.jpg,
+            "detections": [detection.to_dict() for detection in self.detections],
+        }
 
 # Kafka Consumer
 class KafkaImageConsumer:
@@ -151,7 +170,7 @@ class KafkaImageConsumer:
         
         logger.debug("Starting Kafka Consumer thread")
         # start monitoring for kafka messages in a separate thread
-        threading.Thread(target=self.run).start()
+        threading.Thread(target=self.run, daemon=True).start()
 
     def run(self):
         try:
@@ -167,22 +186,42 @@ class KafkaImageConsumer:
 class KafkaResultProducer:
     KAFKA_CONNECTION_STRING: str = os.getenv("KAFKA_CONNECTION_STRING", "localhost:9092")
     topic: str = os.getenv("PRODUCER_TOPIC", "camera_object_detection_results")
+    object_detection_result_schema: schema.Schema
+    _producer: KafkaProducer
     def __init__(self):
         try:
-            self.producer = KafkaProducer(bootstrap_servers=[self.KAFKA_CONNECTION_STRING])
+            self._producer = KafkaProducer(bootstrap_servers=[self.KAFKA_CONNECTION_STRING])
             logger.info("Kafka Producer initialized")
         except KafkaError as e:
             logger.error(f"Error initializing Kafka Producer: {e}")
             raise
+        objectDetectionResultSchemaText = Path('ObjectDetectionResult.avsc').read_text()
+        self.object_detection_result_schema = schema.parse(objectDetectionResultSchemaText)
+        
 
-    def send_result(self, result):
+
+    def send_result(self, detection_result: DetectionResult):
+        # Serialize the DetectionResult object to Avro format
+        bytes_writer = io.BytesIO()
+        writer = DataFileWriter(bytes_writer, DatumWriter(), self.object_detection_result_schema)
+        detection_result_dict = detection_result.to_dict()
+        # for debug purposes serialize detection_result_dict to disk using pathlib
+        # Path(f'output',f'detection_result_dict{detection_result.frame_id}.json').write_text(str(detection_result_dict))
         try:
-            result_bytes = str(result).encode()
-            self.producer.send(self.topic, result_bytes)
-            logger.debug(f"Sent result to topic {self.topic}: {result}")
-        except KafkaError as e:
-            logger.error(f"Error sending result to Kafka: {e}")
+            writer.append(detection_result_dict)
+            writer.flush()
+            bytes_writer.seek(0)
+        except Exception as e:
+            logger.error(f'Unable to serialize detection_result_dict : {e}')
+            logger.error(f'detection result dict is {detection_result_dict}')
 
+        try:
+            # Send the serialized Avro data to the Kafka topic
+            future = self._producer.send(self.topic, bytes_writer.read())
+            logger.debug(f"Detection result for {detection_result.frame_id} sent to Kafka topic")
+            return future
+        except KafkaError as e:
+            logger.error(f'Unable to send camera_motion_threshold_exceeded event : {e}')
 # Object Detector
 class ObjectDetector:
     queue: PriorityQueue
@@ -223,7 +262,7 @@ class ObjectDetector:
 
     def process_image(self, item:MotionMessageQueueItem) -> DetectionResult:
         try:
-            image = Image.open(io.BytesIO(item.frame_jpg))
+            image = item.frame_jpg
             inputs = self.image_processor(images=image, return_tensors="pt")
             outputs = self.model(**inputs)
             # Processing logic here...
@@ -276,6 +315,7 @@ def main():
 
             print(f"Processing item from queue: {item.camera_name} {item.guid} {item.creation_timestamp} {item.motion_amount} {item.timeout}")
             detection_result:DetectionResult  = detector.process_image(item)
+            producer.send_result(detection_result)
 
 
 

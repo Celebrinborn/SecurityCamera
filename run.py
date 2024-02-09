18581b281b89a121f52521604b4d22042d94e2f4
Celebrinborn <cameronjunkmail1@gmail.com>
2024-02-09 11:00:32 -0800

    Refactor logging configuration and camera initialization

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run Flask app')
     parser.add_argument('--host', type=str, default='0.0.0.0', help='Host to run the Flask app on')
     parser.add_argument('--port', type=int, default=5000, help='Port to run the Flask app on')
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
 
     # hack to get logging to work as I can't make the logger before running flask
     sys.stderr.write(f'Running Flask app on {args.host}:{args.port}')
